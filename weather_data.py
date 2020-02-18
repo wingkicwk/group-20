228299133a172a9651cdbaf36b007961c6d22694
Yvette-w <31890389+Yvette-w@users.noreply.github.com>
2020-02-18 00:09:12 +0000

    Update weather_data.py

--- weather_data.py
+++ weather_data.py
@@ -68,25 +68,4 @@ try:
 except:
     # Rollback in case there is any error
     conn.rollback()
-#     info_1 = [str(bike_line['number']),str(bike_line['name']),str(bike_line['banking']),str(bike_line['bike_stands'])]
-#     position = bike_line['position']
-# # print(position)
-# #     print(len(position))
-#     for j in range(0,len(position)):
-#         info_2 = [str(position['lat']),str(position['lng'])]
-#         info = info_1[:2] + info_2 + info_1[2:]
-#         # print(info)
-#         sql_insert = "insert into bd (number,name,lat,lng,banking,bike_stands) values (" + "'"+info[0]+"'" +","+ "'"+info[1]+"'" + ","+"'"+info[2]+"'" + ","+"'"+info[3]+"'" + ","+"'"+info[4]+"'" + ","+"'"+info[5]+"'" + ");"
-#         # print(sql_insert)
-#         try:
-#             # 执行sql语句
-#             cur.execute(sql_insert)
-#             # 提交到数据库执行
-#             conn.commit()
-#         except:
-#             # Rollback in case there is any error
-#             conn.rollback()
-#
-#         # 关闭数据库连接,没有close就可以导入？？？
-#     # conn.close()
-#
+
