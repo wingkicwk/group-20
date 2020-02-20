8d7fa10c88195e3cdb595ea4a4a6920273462a4b
Yi <yi.wu1@ucdconnect.ie>
2020-02-20 17:27:48 +0000

    add the time of getting the data to the table, and make the codes tidier

--- weather_data.py
+++ weather_data.py
@@ -4,9 +4,12 @@ import pymysql
 # import cryptography
 # import sqlalchemy
 # from sqlalchemy import create_engine
-import time 
+import time
 import datetime
 import traceback
+
+currentTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+
 
 conn = pymysql.connect(
     host="database-softwareengieeringproject.cuvpbui26dwd.eu-west-1.rds.amazonaws.com",  # mysql服务器地址
@@ -15,11 +18,11 @@ conn = pymysql.connect(
     passwd="comp30830",  # 密码
     db="segroupproject",  # 数据库名称
     charset='utf8',  # 连接编码，根据需要填写
-    )
+)
 cur = conn.cursor()  # 创建并返回游标
 cur.execute("DROP TABLE IF EXISTS weather")
-#create a empty table
-sql_table = "CREATE TABLE weather (lon VARCHAR(100),lat VARCHAR(100),weather_id VARCHAR(100),main VARCHAR(100),description VARCHAR(100),icon VARCHAR(100),base VARCHAR(100),temp VARCHAR(100),feels_like VARCHAR(100),temp_min VARCHAR(100),temp_max VARCHAR(100),pressure VARCHAR(100),humidity VARCHAR(100),visibility VARCHAR(100),speed VARCHAR(100),deg VARCHAR(100),gust VARCHAR(100),clouds_all VARCHAR(100),dt VARCHAR(100),sys_type VARCHAR(100),sys_id VARCHAR(100),sys_country VARCHAR(100),sunrise VARCHAR(100),sunset VARCHAR(100),timezone VARCHAR(100),id VARCHAR(100),name VARCHAR(100),cod VARCHAR(100));"
+# create a empty table
+sql_table = "CREATE TABLE weather (current VARCHAR(100), lon VARCHAR(100),lat VARCHAR(100),weather_id VARCHAR(100),main VARCHAR(100),description VARCHAR(100),icon VARCHAR(100),base VARCHAR(100),temp VARCHAR(100),feels_like VARCHAR(100),temp_min VARCHAR(100),temp_max VARCHAR(100),pressure VARCHAR(100),humidity VARCHAR(100),visibility VARCHAR(100),speed VARCHAR(100),deg VARCHAR(100),gust VARCHAR(100),clouds_all VARCHAR(100),dt VARCHAR(100),sys_type VARCHAR(100),sys_id VARCHAR(100),sys_country VARCHAR(100),sunrise VARCHAR(100),sunset VARCHAR(100),timezone VARCHAR(100),id VARCHAR(100),name VARCHAR(100),cod VARCHAR(100));"
 cur.execute(sql_table)
 
 
@@ -27,8 +30,6 @@ def insertWeatherIntoDB():
     weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=dublin,IE&units=metric&appid=ef2f7a40c765b06a8ab8b9c674fe8808'
     weather_json = requests.get(weather_url).json()
     l = len(weather_json)
-
-
 
     information = []
     info_1 = []
@@ -45,25 +46,47 @@ def insertWeatherIntoDB():
     sys = weather_json['sys']
     t = (str(coordination['lon']), str(coordination['lat']))
 
-    w = (str(weather['id']),str(weather['main']),str(weather['description']),str(weather['icon']), str(weather_json['base']))
-    m = (str(main['temp']), str(main['feels_like']), str(main['temp_min']),str(main['temp_max']),str(main['pressure']),str(main['humidity']))
+    w = (str(weather['id']), str(weather['main']), str(weather['description']), str(weather['icon']),
+         str(weather_json['base']))
+    m = (
+    str(main['temp']), str(main['feels_like']), str(main['temp_min']), str(main['temp_max']), str(main['pressure']),
+    str(main['humidity']))
 
     c = (str(clouds['all']), str(weather_json['dt']))
-    s =(str(sys['type']),str(sys['id']),str(sys['country']),str(sys['sunrise']),str(sys['sunset']))
+    s = (str(sys['type']), str(sys['id']), str(sys['country']), str(sys['sunrise']), str(sys['sunset']))
     tz = str(weather_json['timezone'])
     id = str(weather_json['id'])
     name = str(weather_json['name'])
     cod = str(weather_json['cod'])
-    info_1 = (tz, id, name,cod)
+    info_1 = (tz, id, name, cod)
     try:
-        #Gust is available sometimes so we add gust with try and except block if it exists
-        w2 = (str(weather_json['visibility']), str(wind['speed']),str(wind['deg']),str(wind['gust']))
-        info = t + w  + m + w2+ c + s+info_1
-        sql_insert = "insert into weather (lon,lat,weather_id,main,description,icon,base,temp,feels_like,temp_min,temp_max,pressure,humidity,visibility,speed,deg,gust,clouds_all,dt,sys_type,sys_id,sys_country,sunrise,sunset ,timezone,id,name,cod) values  (" + "'"+info[0]+"'" +","+ "'"+info[1]+"'" + ","+"'"+info[2]+"'" + ","+"'"+info[3]+"'" + ","+"'"+info[4]+"'" + ","+"'"+info[5]+"'"  + ","+"'"+info[6]+"'" + ","+"'"+info[7]+"'" + ","+"'"+info[8]+"'" + ","+"'"+info[9]+"'" + ","+"'"+info[10]+"'" + ","+"'"+info[11]+"'" + ","+"'"+info[12]+"'" + ","+"'"+info[13]+"'" + ","+"'"+info[14]+"'" + ","+"'"+info[15]+"'" + ","+"'"+info[16]+"'" + ","+"'"+info[17]+"'" + ","+"'"+info[18]+"'" + ","+"'"+info[19]+"'" + ","+"'"+info[20]+"'" + ","+"'"+info[21]+"'" + ","+"'"+info[22]+"'" + ","+"'"+info[23]+"'" + ","+"'"+info[24]+"'" + ","+"'"+info[25]+"'" + ","+"'"+info[26]+"'"  + ","+"'"+info[27]+"'"  +  ");"
+        # Gust is available sometimes so we add gust with try and except block if it exists
+        w2 = (str(weather_json['visibility']), str(wind['speed']), str(wind['deg']), str(wind['gust']))
+        info = t + w + m + w2 + c + s + info_1
+        sql_insert = "insert into weather (currnet, lon,lat,weather_id,main,description,icon,base,temp,feels_like,temp_min,temp_max,pressure,humidity,visibility,speed,deg,gust,clouds_all,dt,sys_type,sys_id,sys_country,sunrise,sunset ,timezone,id,name,cod) values  (" + "'" + \
+                     currentTime + "'" + "," + "'" +  info[0] + "'" + "," + "'" + info[1] + "'" + "," + "'" + info[2] + "'" + "," + "'" + info[
+                         3] + "'" + "," + "'" + info[4] + "'" + "," + "'" + info[5] + "'" + "," + "'" + info[
+                         6] + "'" + "," + "'" + info[7] + "'" + "," + "'" + info[8] + "'" + "," + "'" + info[
+                         9] + "'" + "," + "'" + info[10] + "'" + "," + "'" + info[11] + "'" + "," + "'" + info[
+                         12] + "'" + "," + "'" + info[13] + "'" + "," + "'" + info[14] + "'" + "," + "'" + info[
+                         15] + "'" + "," + "'" + info[16] + "'" + "," + "'" + info[17] + "'" + "," + "'" + info[
+                         18] + "'" + "," + "'" + info[19] + "'" + "," + "'" + info[20] + "'" + "," + "'" + info[
+                         21] + "'" + "," + "'" + info[22] + "'" + "," + "'" + info[23] + "'" + "," + "'" + info[
+                         24] + "'" + "," + "'" + info[25] + "'" + "," + "'" + info[26] + "'" + "," + "'" + info[
+                         27] + "'" + ");"
     except:
-        w2 = (str(weather_json['visibility']), str(wind['speed']),str(wind['deg']))
-        info = t + w  + m + w2+ c + s+info_1
-        sql_insert = "insert into weather (lon,lat,weather_id,main,description,icon,base,temp,feels_like,temp_min,temp_max,pressure,humidity,visibility,speed,deg,clouds_all,dt,sys_type,sys_id,sys_country,sunrise,sunset ,timezone,id,name,cod) values  (" + "'"+info[0]+"'" +","+ "'"+info[1]+"'" + ","+"'"+info[2]+"'" + ","+"'"+info[3]+"'" + ","+"'"+info[4]+"'" + ","+"'"+info[5]+"'"  + ","+"'"+info[6]+"'" + ","+"'"+info[7]+"'" + ","+"'"+info[8]+"'" + ","+"'"+info[9]+"'" + ","+"'"+info[10]+"'" + ","+"'"+info[11]+"'" + ","+"'"+info[12]+"'" + ","+"'"+info[13]+"'" + ","+"'"+info[14]+"'" + ","+"'"+info[15]+"'" + ","+"'"+info[16]+"'" + ","+"'"+info[17]+"'" + ","+"'"+info[18]+"'" + ","+"'"+info[19]+"'" + ","+"'"+info[20]+"'" + ","+"'"+info[21]+"'" + ","+"'"+info[22]+"'" + ","+"'"+info[23]+"'" + ","+"'"+info[24]+"'" + ","+"'"+info[25]+"'" + ","+"'"+info[26]+"'"  +  ");"
+        w2 = (str(weather_json['visibility']), str(wind['speed']), str(wind['deg']))
+        info = t + w + m + w2 + c + s + info_1
+        sql_insert = "insert into weather (current, lon,lat,weather_id,main,description,icon,base,temp,feels_like,temp_min,temp_max,pressure,humidity,visibility,speed,deg,clouds_all,dt,sys_type,sys_id,sys_country,sunrise,sunset ,timezone,id,name,cod) values  (" + "'" + \
+                     currentTime + "'" + "," + "'" +  info[0] + "'" + "," + "'" + info[1] + "'" + "," + "'" + info[2] + "'" + "," + "'" + info[
+                         3] + "'" + "," + "'" + info[4] + "'" + "," + "'" + info[5] + "'" + "," + "'" + info[
+                         6] + "'" + "," + "'" + info[7] + "'" + "," + "'" + info[8] + "'" + "," + "'" + info[
+                         9] + "'" + "," + "'" + info[10] + "'" + "," + "'" + info[11] + "'" + "," + "'" + info[
+                         12] + "'" + "," + "'" + info[13] + "'" + "," + "'" + info[14] + "'" + "," + "'" + info[
+                         15] + "'" + "," + "'" + info[16] + "'" + "," + "'" + info[17] + "'" + "," + "'" + info[
+                         18] + "'" + "," + "'" + info[19] + "'" + "," + "'" + info[20] + "'" + "," + "'" + info[
+                         21] + "'" + "," + "'" + info[22] + "'" + "," + "'" + info[23] + "'" + "," + "'" + info[
+                         24] + "'" + "," + "'" + info[25] + "'" + "," + "'" + info[26] + "'" + ");"
 
     try:
         # 执行sql语句
@@ -74,13 +97,14 @@ def insertWeatherIntoDB():
         # Rollback in case there is any error
         conn.rollback()
 
-while True:  
+
+while True:
     try:
         insertWeatherIntoDB()
         # wait for 5 minutes
-        time.sleep(60)
+        time.sleep(60*30)
 
-        
+
     except:
-        #print error messages with traceback if error occurs
+        # print error messages with traceback if error occurs
         print(traceback.format_exc())
