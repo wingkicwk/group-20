9111f90f9b390e93b65612a8a3387de8e6d662be
Yi <yi.wu1@ucdconnect.ie>
2020-04-11 22:52:44 +0100

    test change ip address

--- web/static/initialize.js
+++ web/static/initialize.js
@@ -16,7 +16,7 @@ function initialize() {
 
 
     function showStationMarkers() {
-        let json = $.getJSON("http://127.0.0.1:5000/bikeMix", null, function (obj) {
+        let json = $.getJSON("http://18.203.168.242/bikeMix", null, function (obj) {
 
                 var stations = obj.bikeMix;
                 for (var i = 0; i < stations.length; i++) {
@@ -73,7 +73,7 @@ function showchart(number_id){
         google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
-             var url = 'http://127.0.0.1:5000/dynamic/' + number_id;
+             var url = 'http://18.203.168.242/dynamic/' + number_id;
 
              let json = $.getJSON(url, null, function (obj) {
                  var chart_info = obj.chart_info;
@@ -126,7 +126,7 @@ function showchart_week(number_id){
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-          var url = 'http://127.0.0.1:5000/dynamic/weekdata/' + number_id;
+          var url = 'http://18.203.168.242/dynamic/weekdata/' + number_id;
 
      let json = $.getJSON(url, null, function (obj) {
          var chart_info = obj.chart_info;
